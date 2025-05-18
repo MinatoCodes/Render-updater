cbcdeae8453a863cdfd412a881cf2c4b9cbb075b
MinatoCodes <subinsirmal11@gmail.com>
2025-05-18 12:57:54 +0545

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,15 +1,10 @@
-
-const { program } = require('commander');
-const { updateFiles } = require('./commands');
+#!/usr/bin/env node
+import { program } from 'commander';
+import { update } from './commands.js'; // Note .js extension
 
 program
   .version('1.0.0')
   .command('update')
-  .description('Update whitelisted files')
-  .option('--dry-run', 'Test without changes')
-  .action((options) => {
-    if (options.dryRun) process.env.DRY_RUN = 'true';
-    updateFiles();
-  });
+  .action(update);
 
-program.parse();
+program.parse(process.argv);
